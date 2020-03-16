3fefb1c1395a27e8bf91ea1bed168ed2b47a3321
MAAAARCY <masatarou.1003@icloud.com>
2020-03-16 17:29:30 +0900

    marcy2

--- IdeaTree/settings.py
+++ IdeaTree/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Idea',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'IdeaTree.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join('Idea', 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
